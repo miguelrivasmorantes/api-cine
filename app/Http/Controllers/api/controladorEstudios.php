c4e5833f4a3bdbd37b10642e25fba9feff2fba53
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-01-30 20:32:46 +0100

    feature/create empty files for models

---
+++ app/Http/Controllers/api/controladorEstudios.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Estudio;
+
+class controladorEstudios extends Controller{
+    public function index(){
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\controladorPeliculas;
+use App\Http\Controllers\api\controladorEstudios;
 
 Route::get('/hola', function () {
     return 'Hola Mundo!!!';
 });
 
+// Start: CRUD operations for Películas
 Route::get('/peliculas', [controladorPeliculas::class, 'index']);
 
 Route::get('/peliculas/{id}', [controladorPeliculas::class, 'show']);
 
 Route::post('/peliculas', [controladorPeliculas::class, 'store']);
+// Start: CRUD operations for Películas
+
+
+// Start: CRUD operations for Estudios
+Route::get('/estudios', [controladorEstudios::class, 'index']);
+
+Route::get('/estudios/{id}', [controladorEstudios::class, 'show']);
+
+Route::post('/estudios', [controladorEstudios::class, 'store']);
+
+Route::delete('/estudios/{id}', [controladorEstudios::class, 'destroy']);
+
+Route::patch('/estudios/{id}', [controladorEstudios::class, 'update']);
+// End: CRUD operations for Estudios
