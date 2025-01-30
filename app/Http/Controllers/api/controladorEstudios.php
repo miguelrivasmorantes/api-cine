d38869bbc71cc628897978f3c657f44aec420471
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-01-30 22:54:58 +0100

    feature/create a estudio

--- app/Http/Controllers/api/controladorEstudios.php
+++ app/Http/Controllers/api/controladorEstudios.php
@@ -14,7 +14,7 @@ class controladorEstudios extends Controller{
                 'id' => $estudio->id,
                 'nombre' => $estudio->nombre,
                 'pais' => $estudio->pais,
-                'fundacion' => $estudio->fundacion,
+                'fundacion' => $estudio->fundacion->format('Y-m-d'),
                 'peliculas' => $estudio->peliculas->pluck('titulo'),
             ];
         });
@@ -42,7 +42,7 @@ class controladorEstudios extends Controller{
             'id' => $estudio->id,
             'nombre' => $estudio->nombre,
             'pais' => $estudio->pais,
-            'fundacion' => $estudio->fundacion,
+            'fundacion' => $estudio->fundacion->format('Y-m-d'),
             'peliculas' => $estudio->peliculas->pluck('titulo'),
         ];
 
@@ -52,5 +52,46 @@ class controladorEstudios extends Controller{
         ];
 
         return $data;
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required|string|max:255|unique:estudios,nombre',
+            'pais' => 'required|string|max:255',
+            'fundacion' => 'required|date_format:Y-m-d',
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en la validación de los datos',
+                'errors' => $validator->errors(),
+                'status' => 400,
+            ];
+
+            return $data;
+        }
+
+        $estudio = Estudio::create([
+            'nombre' => $request->nombre,
+            'pais' => $request->pais,
+            'fundacion' => $request->fundacion,
+        ]);
+
+        if(!$estudio){
+            $data = [
+                'message' => 'Error al crear el estudio',
+                'status' => 500,
+            ];
+
+            return $data;
+        }
+
+        $data = [
+            'estudio' => $estudio,
+            'status' => 201,
+        ];
+
+        return response()->json($data, 201);
     }
 }
