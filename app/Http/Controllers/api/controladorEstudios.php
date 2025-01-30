3988566cc54c67740a494317ab14f0af1872b606
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-01-30 21:51:38 +0100

    feature/get all estudios function

--- app/Http/Controllers/api/controladorEstudios.php
+++ app/Http/Controllers/api/controladorEstudios.php
@@ -9,6 +9,25 @@ use App\Models\Estudio;
 
 class controladorEstudios extends Controller{
     public function index(){
-        
+        $estudios = Estudio::with('peliculas')->get()->map(function($estudio) {
+            return [
+                'id' => $estudio->id,
+                'nombre' => $estudio->nombre,
+                'pais' => $estudio->pais,
+                'fundacion' => $estudio->fundacion,
+                'peliculas' => $estudio->peliculas->pluck('titulo'),
+            ];
+        });
+
+        if(!$estudios){
+            return response()->json(['message' => 'No se encontraron estudios', 'status' => 200], 200);
+        }
+
+        $data = [
+            'estudios' => $estudios,
+            'status' => 200,
+        ];
+
+        return $data;
     }
 }
