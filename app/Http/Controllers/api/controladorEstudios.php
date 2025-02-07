857be94fe0673a537abc922e675bfac9bf27b66d
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-02-07 20:51:52 +0100

    feature/filters for estudios

--- app/Http/Controllers/api/controladorEstudios.php
+++ app/Http/Controllers/api/controladorEstudios.php
@@ -8,8 +8,35 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\Estudio;
 
 class controladorEstudios extends Controller{
-    public function index(){
-        $estudios = Estudio::all()->map(function($estudio) {
+    public function index(Request $request){
+
+        $perPage = $request->input('per_page', 10);
+        $page = $request->input('page', 1);
+        $ordenAlfabetico = $request->input('orden_alfabetico');
+        $ordenAntiguedad = $request->input('orden_antiguedad');
+        $nombre = $request->input('nombre');
+        $AntiguedadSuperior = $request->input('antiguedad_superior');
+        $AntiguedadInferior = $request->input('antiguedad_inferior');
+        $pais = $request->input('pais');
+
+        $estudios = Estudio::
+            when($nombre, function ($q) use ($nombre) {$q->where('nombre', 'like', "%{$nombre}%");})->
+            when($pais, function ($q) use ($pais) {$q->where('pais', 'like', "%{$pais}%");})->               
+            when($AntiguedadInferior, function ($q) use ($AntiguedadInferior) {
+                $q->whereRaw("TIMESTAMPDIFF(YEAR, fundacion, CURDATE()) >= ?", [$AntiguedadInferior]);
+            })->
+            when($AntiguedadSuperior, function ($q) use ($AntiguedadSuperior) {
+                $q->whereRaw("TIMESTAMPDIFF(YEAR, fundacion, CURDATE()) <= ?", [$AntiguedadSuperior]);
+            })->
+            when($ordenAlfabetico, function ($q) use ($ordenAlfabetico){
+                $q->orderBy('nombre', $ordenAlfabetico);
+            })->
+            when($ordenAntiguedad, function ($q) use ($ordenAntiguedad){
+                $q->orderBy('fundacion', $ordenAntiguedad);
+            })->
+            orderBy('id', 'desc')->
+            paginate($perPage, ['*'], 'page', $page)->
+            map(function($estudio){
             return [
                 'id' => $estudio->id,
                 'nombre' => $estudio->nombre,
